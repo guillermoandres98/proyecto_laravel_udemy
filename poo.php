5a4fef16245b54cb0f98e9d1c47745b72bd150b6
Guillermo Fuentes <guillermo.andresv98@gmail.com>
2024-08-15 06:57:02 +0100

    Actualizacion de archivos

--- poo.php
+++ poo.php
@@ -2,9 +2,33 @@
     //PROGRAMACION ORIENTADA A OBJETOS
 
     //Declarar una clase
+    /*
+        -Metodos
+        -Atributos
+        -Constructor
+        -Metodos estaticos
+    */ 
+    class Miclase{
 
+        //atributos con modificadores de acceso
+        public $nombre;
+        public $corrreo;
+        public $edad;
+        //constructor
+        public function __construct($nombre, $corrreo, $edad){
+            $this-> nombre = $nombre;
+            $this-> corrreo =$corrreo;
+            $this-> edad = $edad;
+        }
+        //metodos
+        public function getNombre(){
+            return $this->nombre;
+        }
+        //metodos estaticos
+    }
 
     //instancia (llamar) a la $variable-> variable de instancia
-
+    $miClase = new Miclase("guillermo","hola@hola","14");
+    echo $miClase->getNombre();
 
 ?>
